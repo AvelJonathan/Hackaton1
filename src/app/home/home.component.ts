8d1773b2d7ab392daadd30ebf76a24445246dfb6
AvelJonathan <jonathan.avel8@gmail.com>
2018-04-05 21:11:51 +0200

    starting API

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
   }
+  ngOnInit(): void {
 
+    interface UserResponse {
+      id: string;
+      name: string;
+      images: any;
+    }
+    this.http.get<UserResponse>('https://akabab.github.io/superhero-api/api/id/1.json').subscribe(data => {
+      console.log(data);
+      console.log('User ID: ' + data.id);
+      console.log('Name: ' + data.name);
+      console.log('Image: ' + data.images.md);
+    });
+  }
 }
