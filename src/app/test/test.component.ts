4c2c1d9816c6e439b7940ac8238aed2a9cd0115f
AvelJonathan <jonathan.avel8@gmail.com>
2018-04-06 06:00:34 +0200

    add selectedHero

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,14 +8,22 @@ import { ApiService } from '../api.service';
 })
 export class TestComponent implements OnInit {
   heroes; // all the heroes
-  hero; // hero par rapport a son id
+  // /selectPlayer = null;*/
+  selectedPlayer = [];
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
     this.apiService.getHeroes().subscribe(heroes => {
-      // console.log(heroes);
+      console.log(heroes);
       this.heroes = heroes;
     });
   }
-
+  getHero(param, player) {
+    console.log(param);
+    this.selectedPlayer[player] = this.heroes[param];
+    // return this.heroes[param].images.sm;
+  }
+  /*getPerso(param) {
+    this.selectPlayer = this.heroes(param);
+  }*/
 }
